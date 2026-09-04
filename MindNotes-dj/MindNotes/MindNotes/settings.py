88d5edca6283dfe7199f032b09d365b58e2277bb
god-the-coder <mrgodninjaq@gmail.com>
2026-09-04 23:23:37 +0530

    preparing app for production

--- MindNotes-dj/MindNotes/MindNotes/settings.py
+++ MindNotes-dj/MindNotes/MindNotes/settings.py
@@ -1,20 +1,19 @@
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-local-dev-key"
+)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7z_j^p67+dzts8x!+0rf$menypr_rs^ygsy-!1*65a(*#&d2vd'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",")
 
 
 # Application definition
